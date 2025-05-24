e23afbe927045b456c452821619e308b9c762d3b
jackbaggins <jeffers.jake@gmail.com>
2025-05-24 18:28:29 -0400

    housekeeping changes to util

--- util.c
+++ util.c
@@ -115,7 +115,7 @@ err:
 SRD_PRIV int py_attr_as_strlist(PyObject *py_obj, const char *attr, GSList **outstrlist)
 {
 	PyObject *py_list;
-	ssize_t idx;
+	Py_ssize_t i;
 	int ret;
 	char *outstr;
 	PyGILState_STATE gstate;
@@ -139,10 +139,10 @@ SRD_PRIV int py_attr_as_strlist(PyObject *py_obj, const char *attr, GSList **out
 
 	*outstrlist = NULL;
 
-	for (idx = 0; idx < PyList_Size(py_list); idx++) {
-		ret = py_listitem_as_str(py_list, idx, &outstr);
+	for (i = 0; i < PyList_Size(py_list); i++) {
+		ret = py_listitem_as_str(py_list, i, &outstr);
 		if (ret < 0) {
-			srd_dbg("Couldn't get item %zd.", idx);
+			srd_dbg("Couldn't get item %" PY_FORMAT_SIZE_T "d.", i);
 			goto err;
 		}
 		*outstrlist = g_slist_append(*outstrlist, outstr);
@@ -217,9 +217,8 @@ err:
 SRD_PRIV int py_listitem_as_str(PyObject *py_obj, Py_ssize_t idx,
 				char **outstr)
 {
-	PyGILState_STATE gstate;
-	ssize_t item_idx;
 	PyObject *py_value;
+	PyGILState_STATE gstate;
 
 	gstate = PyGILState_Ensure();
 
@@ -228,9 +227,8 @@ SRD_PRIV int py_listitem_as_str(PyObject *py_obj, Py_ssize_t idx,
 		goto err;
 	}
 
-	item_idx = idx;
-	if (!(py_value = PyList_GetItem(py_obj, item_idx))) {
-		srd_dbg("Couldn't get list item %zd.", item_idx);
+	if (!(py_value = PyList_GetItem(py_obj, idx))) {
+		srd_dbg("Couldn't get list item %" PY_FORMAT_SIZE_T "d.", idx);
 		goto err;
 	}
 
@@ -305,7 +303,7 @@ err:
  *
  * @private
  */
-SRD_PRIV int py_pydictitem_as_long(PyObject *py_obj, PyObject *py_key, int64_t *out)
+SRD_PRIV int py_pydictitem_as_long(PyObject *py_obj, PyObject *py_key, uint64_t *out)
 {
 	PyObject *py_value;
 	PyGILState_STATE gstate;
@@ -330,7 +328,7 @@ SRD_PRIV int py_pydictitem_as_long(PyObject *py_obj, PyObject *py_key, int64_t *
 		goto err;
 	}
 
-	*out = PyLong_AsLongLong(py_value);
+	*out = PyLong_AsUnsignedLongLong(py_value);
 
 	PyGILState_Release(gstate);
 
@@ -387,7 +385,7 @@ SRD_PRIV int py_str_as_str(PyObject *py_str, char **outstr)
 
 /**
  * Convert a Python list of unicode strings to a C string vector.
- * On success, a pointer to a newly allocated NUL-terminated array of
+ * On success, a pointer to a newly allocated NULL-terminated array of
  * allocated C strings is written to @a out_strv. The caller must g_free()
  * each string and the array itself.
  *
